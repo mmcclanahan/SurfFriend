8a2cfcd74bd73a310366e5c289516ffae71407d8
mmcclanahan <mmmcclan365@gmail.com>
2024-07-22 11:04:07 -0700

    display time in users timezone on card

--- client/src/components/Calendar/SessionCard.tsx
+++ client/src/components/Calendar/SessionCard.tsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import { Session } from "../../types/types";
 import { SessionCondition } from "./SessionCondition";
+import { parseISO, format } from "date-fns";
 
 export const SessionCard = ({ session }: { session: Session }) => {
   const [showConditions, setShowConditions] = useState(false);
   const toggleConditions = () => setShowConditions(!showConditions);
+
+  const time = format(parseISO(session.createdAt), "hh:mm a");
+
   return (
     <div
       key={session.createdAt}
       className="border border-black"
       onClick={toggleConditions}
     >
-      <p>Date: {new Date(session.createdAt).toLocaleDateString()}</p>
+      <p>Time: {time}</p>
       <p>Spot Name: {session.spotName}</p>
       <p>City: {session.city}</p>
       <p>Rating: {session.rating}</p>
